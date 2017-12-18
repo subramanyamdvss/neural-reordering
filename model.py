3db271893505180e836f04eff0db5223a6ccf30f
subramanyamdvss <subramanyamdvss@gmail.com>
2017-12-18 23:10:55 +0530

    updated evaluation metrics

--- model.py
+++ model.py
@@ -8,8 +8,9 @@ from torch.utils.data import Dataset, DataLoader
 import pickle
 import gc
 import math
-hyper = {'temp':0.01,'lr':0.07,'numepochs':1000,'optim':'momentum','batchsize':32,'lrdecaystepsize':30,'lrdecay':0.5,'weightnorm':3,'momentum':0.9,'weightdecay':0.0}
-lcontrols = {'valstepsize':10,'savedir':'models2/','epoch':10000,'PATH':'models2/22-train-0.073201-0.879936-val-0.084457-0.866586.model4'}
+import re
+hyper = {'temp':0.01,'lr':0.035,'numepochs':1000,'optim':'momentum','batchsize':32,'lrdecaystepsize':30,'lrdecay':0.5,'weightnorm':3,'momentum':0.9,'weightdecay':0.0}
+lcontrols = {'valstepsize':1,'savedir':'models2/','epoch':1,'PATH':'models2/46-train-0.128189-0.001782-val-0.127008-0.821692.model5'}
 torch.backends.cudnn.benchmark= True
 
 def weightini(modules,nl):
@@ -97,7 +98,9 @@ class Net(nn.Module):
     
 
 net = Net()
-# net.load_state_dict(torch.load(lcontrols['PATH']))
+net.load_state_dict(torch.load(lcontrols['PATH']))
+# vali = int(lcontrols['PATH'].split('/')[1].split('-'))+1
+vali = 0
 net.zero_grad()
 
 #defining the loss function
@@ -184,7 +187,7 @@ net.cuda()
 optimizer = torch.optim.SGD(net.parameters(), lr=hyper['lr'],momentum=hyper['momentum'], weight_decay=hyper['weightdecay'])
 optimizer.zero_grad() 
 #this is going to take a step before every epoch starts
-#scheduler = torch.optim.lr_scheduler.StepLR(optimizer, hyper['lrdecaystepsize'], gamma=hyper['lrdecay'], last_epoch=-1)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, hyper['lrdecaystepsize'], gamma=hyper['lrdecay'], last_epoch=-1)
 
 
 #defining dataloaders for train val and test datasets.
@@ -194,6 +197,75 @@ valloader = DataLoader(valset, batch_size=hyper['batchsize'],shuffle=False, num_
 epoch = 0
 nsteps = len(trainset)/hyper['batchsize']
 batchsize = hyper['batchsize']
+
+# coding: utf-8
+
+# In[53]:
+
+sentence_gt = '1 3 4'
+
+
+# In[54]:
+
+sentence_pred = '2 3 1 4'
+
+
+# In[64]:
+
+regex1 = re.compile('[ ]+')
+
+
+# In[72]:
+
+def Pairwise_Metric(ground_truth, predicted):
+    # Find the skip bigram pairs
+    #list_gt = regex1.split(ground_truth)
+    #list_pred = regex1.split(predicted)
+    list_gt = ground_truth
+    list_pred = predicted
+    skip_bigrams_gt = set([(list_gt[i], list_gt[j]) for i in range(len(list_gt)) for j in range(i+1, len(list_gt))])
+    skip_bigrams_pred = set([(list_pred[i], list_pred[j]) for i in range(len(list_pred)) for j in range(i+1, len(list_pred))])
+
+    # Calculate and return precision, recall, f-score
+    P = float(len(skip_bigrams_gt.intersection(skip_bigrams_pred))/len(skip_bigrams_pred))
+
+    R = float(len(skip_bigrams_gt.intersection(skip_bigrams_pred))/len(skip_bigrams_gt))
+
+    fs = 2*P*R/max((P+R),1e-8)
+    
+    return P,R,fs
+
+def lcs_metric(a, b):
+    # The dynamic programming algo for longest common subsequence
+    lengths = np.zeros((len(a) + 1, len(b) + 1))
+
+    # row 0 and column 0 are initialized to 0 already
+    for i, x in enumerate(a):
+        for j, y in enumerate(b):
+            if x == y:
+                lengths[i+1, j+1] = lengths[i, j] + 1
+            else:
+                lengths[i+1, j+1] = max(lengths[i+1, j], lengths[i, j+1])
+
+    return float(lengths[-1, -1])
+
+
+def Perfect_Match(ground_truth, predicted):
+    # Perfect match b/w ground_truth sequence and predicted sequence
+    # list_gt = regex1.split(ground_truth)
+    # list_pred = regex1.split(predicted)
+    list_gt = ground_truth
+    list_pred = predicted
+    pm = 0
+    for i, gx in enumerate(list_gt):
+        if gx == list_pred[i]:
+            pm += 1
+    pm = pm/float(len(list_pred))
+    return pm
+
+
+ 
+
 
 def perfmetric(calcsent,instance):
     #p-all
@@ -231,6 +303,83 @@ def inference(instance,val=False):
     loss = criterion(x,groundsent.view(-1))
     return calcsent,loss
 
+def validationm(k):
+    metrics = torch.Tensor(6).fill_(0)
+    for i_batch,sent_batch in enumerate(valloader):
+        if(len(sent_batch['len'])!=hyper['batchsize']):
+            continue
+        groundsent = sent_batch['groundsent'].cuda()
+        shuffsent = sent_batch['shuffsent'].cuda()
+        groundtruth = sent_batch['groundtruth'].cuda()
+        groundsent = Variable(groundsent)
+        shuffsent = Variable(shuffsent)
+        groundtruth = Variable(groundtruth)
+        x,calcsent = net.forward(shuffsent)
+        # x = softy(x).view(hyper['batchsize'],80,81)
+        loss = criterion(x,groundtruth.view(-1))
+        loss = float(loss.data)
+        #print(x.size(),groundsent.view(-1).size())
+        #x->32x81 groundtruth.view(-1)->32*81
+        x = x.view(hyper['batchsize'],80,81)
+        leng = sent_batch['len'].cuda()    
+        
+        
+        tot =int(leng.sum())
+        fs = 0
+        P = 0
+        R = 0
+        lcs = 0
+        pm = 0
+        probs = x.data
+        probs = probs.view(-1,81)
+        _,indices = probs.topk(2,dim=1)
+        mask = torch.eq(indices[:,0],torch.cuda.LongTensor(hyper['batchsize']*80).fill_(80)).type(torch.cuda.ByteTensor)
+        calcsent[mask] = indices[:,1][mask]
+        calcsent = calcsent.data.view(hyper['batchsize'],-1)
+        for i in range(hyper['batchsize']):
+            #print(calcsent[i,:leng[i]].size(),groundsent[i,:leng[i]].size())
+            #print(type(calcsent[i,:leng[i]]),type(groundsent[i,:leng[i]]))
+            #80x81
+            # print(probs.size())
+            
+            # print(indices.size())
+            
+            # msel = torch.masked_select(torch.arange(80).type(torch.cuda.LongTensor),mask)
+            
+
+            # testleng = leng[i]
+            # print(int(testleng))
+            # calcsent = beamdecode(probs,int(testleng),k)
+            #print(torch.nonzero(calcsent[i]-80).squeeze(1))
+            #minsent = calcsent[i].index_select(0,torch.nonzero(calcsent[i]-80).squeeze(1)) 
+            #print(minsent)
+            # for i in range(80):
+            #     if i=
+            tmp = torch.index_select(shuffsent.data[i],0,calcsent[i,:leng[i]])
+            a = list(tmp)
+            #pada = list(torch.cat((tmp,torch.cuda.LongTensor(80-len(minsent)).fill_(0)),0))
+            b = list(torch.index_select(shuffsent.data[i],0,groundtruth[i,:leng[i]].data))
+            # sb = calcsent[i,:leng[i]]-groundtruth[i,:leng[i]].data
+            # perf += (int(leng[i])-len(torch.nonzero(sb)))
+            p,r,f = Pairwise_Metric(b,a)
+            P+=p
+            R+=r
+            fs+=f
+            lcs+=lcs_metric(b,a)/float(leng[i])
+            pm+=Perfect_Match(b,a)
+        bmetrics = torch.Tensor([0,fs,P,R,lcs,pm]).div_(hyper['batchsize'])
+        bmetrics[0] = loss
+        print('Validation metrics:[loss,F,P,R,lcs,pm] %d'%(i_batch),list(bmetrics))
+        metrics = metrics+bmetrics
+        
+        # perf,tot = perfmetric(calcsent.view(hyper['batchsize'],-1),sent_batch)
+        # totloss+=float(loss.data)
+        # totperf+=perf
+        # totmatches+=tot
+    return metrics.div_(i_batch)
+    #eturn float(totperf)/totmatches,float(totloss)/(i_batch)
+
+
 def validation():
     nsteps = len(valset)/hyper['batchsize'] 
     totloss = 0
@@ -267,7 +416,7 @@ def validation():
     return float(totperf)/totmatches,float(totloss)/(i_batch)
 
 
-
+# validationm(1)
 
 # def l2constraint(modu):
 #     #normalize the conv layers
@@ -275,7 +424,7 @@ def validation():
 #     if cn > hyper['weightnorm']:
 #         modu.weight = modu.weight.div(cn.expand_as(modu.weight))
 
-vali = 1
+
 f = open('val2.log','w+')
 trperf = 0
 trtot = 0
@@ -285,7 +434,8 @@ i_batch = 0
 travgloss = 0.0
 travgperf = 0.0
 for epoch in range(lcontrols['epoch']):
-    
+    metrics = torch.Tensor(6).fill_(0)
+    trcnt = 0
     for j,sent_batch in enumerate(trainloader):
         if(len(sent_batch['len'])!=hyper['batchsize']): 
             continue
@@ -303,27 +453,77 @@ for epoch in range(lcontrols['epoch']):
         j2 = groundsent.size()[-1]
         leng = sent_batch['len'].cuda()
         perf = 0    
-        calcsent = calcsent.data.view(hyper['batchsize'],-1)
+        # calcsent = calcsent.data.view(hyper['batchsize'],-1)
         tot =int(leng.sum())
+        
+        # for i in range(hyper['batchsize']):
+        #     #print(calcsent[i,:leng[i]].size(),groundsent[i,:leng[i]].size())
+        #     #print(type(calcsent[i,:leng[i]]),type(groundsent[i,:leng[i]]))
+        #     sb = calcsent[i,:leng[i]]-groundtruth[i,:leng[i]].data
+        #     perf += (int(leng[i])-len(torch.nonzero(sb)))
+        # calcsent = calcsent.data.view(hyper['batchsize'],-1)
+        tot =int(leng.sum())
+        fs = 0
+        P = 0
+        R = 0
+        lcs = 0
+        pm = 0
+        trcnt+=1
+        calcsent = calcsent.data
+        probs = x.data.view(-1,81)
+        _,indices = probs.topk(2,dim=1)
+        mask = torch.eq(indices[:,0],torch.cuda.LongTensor(hyper['batchsize']*80).fill_(80)).type(torch.cuda.ByteTensor)
+        calcsent[mask] = indices[:,1][mask]
+        calcsent = calcsent.view(hyper['batchsize'],-1)
         for i in range(hyper['batchsize']):
             #print(calcsent[i,:leng[i]].size(),groundsent[i,:leng[i]].size())
             #print(type(calcsent[i,:leng[i]]),type(groundsent[i,:leng[i]]))
-            sb = calcsent[i,:leng[i]]-groundtruth[i,:leng[i]].data
-            perf += (int(leng[i])-len(torch.nonzero(sb)))
-        matches = perf
-        trperf+=perf
-        trtot+=tot
-        totloss+=float(loss.data)
-        avgloss = totloss/(i_batch+1)
-        avgperf = float(trperf)/trtot
-        perf = perf/tot
+            # probs = x[i].data#80x81
+            # print(probs.size())
+            # _,indices = probs.topk(2,dim=1)
+            # print(indices.size())
+            # mask = torch.eq(indices[:,0],torch.cuda.LongTensor(80).fill_(80)).type(torch.cuda.ByteTensor)
+            # msel = torch.masked_select(torch.arange(80).type(torch.cuda.LongTensor),mask)
+            # calcsent[i][mask] = indices[:,1][mask]
+
+            # testleng = leng[i]
+            # print(int(testleng))
+            # calcsent = beamdecode(probs,int(testleng),k)
+            #print(torch.nonzero(calcsent[i]-80).squeeze(1))
+            #minsent = calcsent[i].index_select(0,torch.nonzero(calcsent[i]-80).squeeze(1)) 
+            #print(minsent)
+            # for i in range(80):
+            #     if i=
+            tmp = torch.index_select(shuffsent.data[i],0,calcsent[i,:leng[i]])
+            a = list(tmp)
+            #pada = list(torch.cat((tmp,torch.cuda.LongTensor(80-len(minsent)).fill_(0)),0))
+            b = list(torch.index_select(shuffsent.data[i],0,groundtruth[i,:leng[i]].data))
+            # sb = calcsent[i,:leng[i]]-groundtruth[i,:leng[i]].data
+            # perf += (int(leng[i])-len(torch.nonzero(sb)))
+            p,r,f = Pairwise_Metric(b,a)
+            P+=p
+            R+=r
+            fs+=f
+            lcs+=lcs_metric(b,a)/float(leng[i])
+            pm+=Perfect_Match(b,a)
+        bmetrics = torch.Tensor([0,fs,P,R,lcs,pm]).div_(hyper['batchsize'])
+        bmetrics[0] = float(loss)
+        # print('metrics:[loss,F,P,R,lcs,pm]',list(bmetrics))
+        metrics = metrics+bmetrics
+        # matches = perf
+        # trperf+=perf
+        # trtot+=tot
+        # totloss+=float(loss.data)
+        # avgloss = totloss/(i_batch+1)
+        # avgperf = float(trperf)/trtot
+        # perf = perf/tot
         loss.backward()
         optimizer.step()
         optimizer.zero_grad() 
         i_batch+=1
-        print("(epoch,step,loss,perf,avgloss,avgperf)",epoch,i_batch,float(loss.data),perf,avgloss,avgperf)
-        travgloss = avgloss
-        travgperf = avgperf
+        print('Training metrics:[epoch,step,loss,F,P,R,lcs,pm] %d %d'%(epoch,i_batch),list(bmetrics))
+    travgloss = float(loss)
+    trainmetrics = metrics.div_(trcnt)
         # prints currently alive Tensors and Variables
         # m  = 0
         # try:
@@ -353,14 +553,12 @@ for epoch in range(lcontrols['epoch']):
             
             net.eval()
             net.training = False
-            pall,avgloss = validation()
+            valmetrics = validationm(1)
             net.training = True
             net.train()
-            print("Validation-metrics-(vali,epoch,step,pall,avgloss)=(%d,%d,%d,%f,%f)"%(vali,epoch,i_batch,pall,avgloss))
-            f.write("Validation-metrics-(vali,epoch,step,pall,avgloss)=(%d,%d,%d,%f,%f)"%(vali,epoch,i_batch,pall,avgloss))
-            print("saving-model....")   
-            f.write("saving-model....")
-            torch.save(net.state_dict(),lcontrols['savedir']+'%d-train-%f-%f-val-%f-%f.model5'%(vali,travgperf,travgloss,pall,avgloss))
+            torch.save(net.state_dict(),lcontrols['savedir']+'%d-train-%f-%f-%f-%f-%f-%f-val-%f-%f-%f-%f-%f-%f.model5'%(vali,float(trainmetrics[0]),float(trainmetrics[1])\
+                ,float(trainmetrics[2]),float(trainmetrics[3]),float(trainmetrics[4]),float(trainmetrics[5]),float(valmetrics[0]),float(valmetrics[1])\
+                ,float(valmetrics[2]),float(valmetrics[3]),float(valmetrics[4]),float(valmetrics[5])))
             vali+=1
         trperf = 0
         trtot = 0
